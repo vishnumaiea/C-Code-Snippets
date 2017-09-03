7c39a71bde9d6d582148acdd47510e9db49d5fa8
Vishnu M Aiea <vishnumaiea@gmail.com>
2017-09-03 22:17:14 +0530

    Added comments

--- Anything-To-Binary.cpp
+++ Anything-To-Binary.cpp
@@ -14,25 +14,27 @@
 
 using namespace std;
 
-void reverseString (char* sourceString) {
-    char temp;
+//the following function reverses the binary string
+//becasue LSB will be on the left side when binaryBuffer is created.
+void reverseString (char* sourceString) { //modify the source string
+    char temp; //copy the char
     int arraySize = strlen(sourceString);
     for(int i=0; i<arraySize/2; i++) {
-        temp = sourceString[i];
-        sourceString[i] = sourceString[(arraySize-1)-i];
-        sourceString[(arraySize-1)-i] = temp;
+        temp = sourceString[i]; //copy the char
+        sourceString[i] = sourceString[(arraySize-1)-i]; //switch the digits from right to left
+        sourceString[(arraySize-1)-i] = temp; //copy the left half char to right
     }
 }
 
 int main() {
     int i = 0;
-    char binaryBuffer[65];
-    long long number = 6345353453453453324234339;
-    while (number>0) {
-        sprintf(&binaryBuffer[i++],"%d",(number & 1));
-        number >>= 1;
+    char binaryBuffer[65]; //65 bits
+    long long number = 6345353453453453324234339; //a really large number
+    while (number>0) { //loop until number becomes 0
+        sprintf(&binaryBuffer[i++],"%d",(number & 1)); //AND each LSB with 1
+        number >>= 1; //shift the number to right
     }
-    reverseString(binaryBuffer);
-    cout << binaryBuffer;
+    reverseString(binaryBuffer); //reverse the string
+    cout << binaryBuffer; //print the boanry string in the right format
 }
 
