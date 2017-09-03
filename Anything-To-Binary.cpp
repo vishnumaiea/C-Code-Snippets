133a885bdf3ab101bcb3b7ce7df303ec1d1ebd60
Vishnu M Aiea <vishnumaiea@gmail.com>
2017-09-03 22:20:35 +0530

    Update Anything-To-Binary.cpp

--- Anything-To-Binary.cpp
+++ Anything-To-Binary.cpp
@@ -19,7 +19,7 @@ using namespace std;
 void reverseString (char* sourceString) { //modify the source string
     char temp; //copy the char
     int arraySize = strlen(sourceString);
-    for(int i=0; i<arraySize/2; i++) {
+    for(int i=0; i<arraySize/2; i++) { //
         temp = sourceString[i]; //copy the char
         sourceString[i] = sourceString[(arraySize-1)-i]; //switch the digits from right to left
         sourceString[(arraySize-1)-i] = temp; //copy the left half char to right
@@ -28,7 +28,7 @@ void reverseString (char* sourceString) { //modify the source string
 
 int main() {
     int i = 0;
-    char binaryBuffer[65]; //65 bits
+    char binaryBuffer[65]; //65 bits - why odd number is because we can split the binary string half
     long long number = 6345353453453453324234339; //a really large number
     while (number>0) { //loop until number becomes 0
         sprintf(&binaryBuffer[i++],"%d",(number & 1)); //AND each LSB with 1
