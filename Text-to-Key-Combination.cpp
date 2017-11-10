6e5a7fe18efa28e8873a281a122c880ba4ce3961
Vishnu M Aiea <vishnumaiea@gmail.com>
2017-11-10 17:36:09 +0530

    Create Text-to-Key-Combination.cpp

---
+++ Text-to-Key-Combination.cpp
@@ -0,0 +1,24 @@
+//===========================================================================//
+//                                                                           //
+//  A small program to convert uppercase letter text to key combinations     //
+//  Author : Vishnu M Aiea                                                   //
+//  Date created : IST 5:35 PM 10-11-2017, Friday                            //
+//===========================================================================//
+
+#include <iostream>
+using namespace std;
+
+int m [27] = {21,22,23,31,32,33,41,42,43,51,52,53,61,62,63,71,72,73,74,81,82,83,91,92,93,94};
+
+int main() {
+    int i,j,k;
+    char* a;
+    cin>> a;
+    for(i=0; i < sizeof(a); i++) {
+        j = m[a[i] - 65];
+        for(k=0; k < (j-((j/10)*10)); k++)
+        cout<< j/10;
+        cout<< " ";
+    }
+    return 0;
+}
