37a8ac95a39651f52ea99b6098aa0121c9221f85
Vishnu M Aiea <vishnumaiea@gmail.com>
2017-11-10 17:37:03 +0530

    Update Text-to-Key-Combination.cpp

--- Text-to-Key-Combination.cpp
+++ Text-to-Key-Combination.cpp
@@ -2,7 +2,9 @@
 //                                                                           //
 //  A small program to convert uppercase letter text to key combinations     //
 //  Author : Vishnu M Aiea                                                   //
+//  GitHub : https://github.com/vishnumaiea/                                 //
 //  Date created : IST 5:35 PM 10-11-2017, Friday                            //
+//                                                                           //
 //===========================================================================//
 
 #include <iostream>
